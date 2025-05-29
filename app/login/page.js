02d201d565ad814a0a8d076edd8c2b859e63b903
sathvik89 <koriginjasathvik@gmail.com>
2025-05-29 22:33:42 +0530

    show 'Hello [User]' on home page using server session

--- app/login/page.js
+++ app/login/page.js
@@ -4,13 +4,39 @@ import { signIn } from "next-auth/react";
 
 export default function LoginPage() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <button
-        onClick={() => signIn("google")}
-        className="bg-blue-600 cursor-pointer text-white px-6 py-2 rounded hover:bg-blue-700"
-      >
-        @ Sign-in with Google
-      </button>
+    <div className="min-h-screen px-4 flex items-center justify-center bg-gradient-to-br from-orange-50 to-pink-100">
+      <div className="bg-white rounded-xl shadow-lg p-10 w-full max-w-md text-center">
+        <div className="flex justify-center mb-6">
+          <div className="text-5xl">🍕</div>
+        </div>
+
+        <h1 className="text-2xl font-bold text-gray-900 mb-2">
+          Welcome to PizzaHub
+        </h1>
+
+        <p className="text-gray-600 mb-6">
+          Sign in to manage your pizza business
+        </p>
+
+        <button
+          onClick={() => signIn("google", { callbackUrl: "/" })}
+          className="flex items-center cursor-pointer justify-center gap-3 w-full bg-gradient-to-r from-orange-500 to-red-500 text-white py-3 rounded-lg text-sm font-medium shadow hover:from-orange-600 hover:to-red-600 transition"
+        >
+          <span className="text-xl">＠</span> Continue with Google
+        </button>
+
+        <p className="text-xs text-gray-400 mt-6">
+          By signing in, you agree to our{" "}
+          <a href="#" className="underline">
+            Terms of Service
+          </a>{" "}
+          and{" "}
+          <a href="#" className="underline">
+            Privacy Policy
+          </a>
+          .
+        </p>
+      </div>
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,22 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 
-export default function Home() {
-  redirect("/login");
+export default async function Home() {
+  const response = await getServerSession(authOptions); // check if user is logged in
+
+  if (!response) {
+    redirect("/login");
+  }
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-green-50 px-4">
+      <div className="bg-white shadow-lg rounded-2xl p-10 text-center space-y-4">
+        <h1 className="text-3xl font-extrabold text-gray-800">
+          Hello, {response.user.name}! 👋
+        </h1>
+        <p className="text-gray-600">You're successfully logged in.</p>
+      </div>
+    </div>
+  );
 }
